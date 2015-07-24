f090950823b1a0031d4d4ffbbeabb8b87627d967
Elena <htoropova@gmail.com>
2015-07-24 16:01:36 +0300

    Commit home task #1

--- src/test/java/htoropova/TestBase.java
+++ src/test/java/htoropova/TestBase.java
@@ -5,6 +5,8 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.Proxy;
+import org.openqa.selenium.remote.CapabilityType;
 
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
@@ -22,16 +24,22 @@ public class TestBase {
 	protected String gridHubUrl;
 
 	protected String baseUrl;
+	
+	protected String myProxy;
 
 	@BeforeClass
 	public void init() {
 		baseUrl = PropertyLoader.loadProperty("site.url");
 		gridHubUrl = PropertyLoader.loadProperty("grid2.hub");
+		myProxy = PropertyLoader.loadProperty("firefox.proxy");
 
 		DesiredCapabilities capabilities = new DesiredCapabilities();
 		capabilities.setBrowserName(PropertyLoader.loadProperty("browser.name"));
 		capabilities.setVersion(PropertyLoader.loadProperty("browser.version"));
+		capabilities.setCapability(CapabilityType.PROXY, new Proxy().setHttpProxy(myProxy));
+		
 		String platform = PropertyLoader.loadProperty("browser.platform");
+		 
 		if (!(null == platform || "".equals(platform))) {
 			capabilities.setPlatform(Platform.valueOf(PropertyLoader.loadProperty("browser.platform")));
 		}
